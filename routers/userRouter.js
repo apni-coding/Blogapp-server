226a0a8a77e2e4aba3d6e9208898f8b90df02b5d
apni-coding <apnicoding72@gmail.com>
2024-01-13 00:03:17 +0530

    joi validation

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const { register, login, getUser, getAuthors, changeAvtar, editUser } = require('../controllers/userController');
+const { userValidation } = require('../middlewares/userValidation');
 
 const userRouter = express.Router();
 
-userRouter.post('/register', register);
+userRouter.post('/register', userValidation, register);
 userRouter.post('/login', login);
 userRouter.get('/:id', getUser);
 userRouter.get('/', getAuthors);
