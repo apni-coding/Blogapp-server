08ecc7dde361eb7a99d726af99bcf2ded00dc3b8
apni-coding <apnicoding72@gmail.com>
2024-01-17 22:01:13 +0530

    login

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,7 @@
 const { userModel } = require("../models/userModel")
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken')
+require('dotenv').config()
 
 const register = async (req, res) => {
     try {
@@ -27,8 +29,31 @@ const register = async (req, res) => {
     }
 }
 
-const login = (req, res) => {
-    res.send('this is from login')
+const login = async (req, res) => {
+    try {
+        const {email, password} = req.body;
+        if(!email || !password){
+            return res.status(422).json({error:"All Fields are Required"})
+        }
+        const newEmail = email.toLowerCase();
+        const isuser = await userModel.findOne({ email: newEmail });
+        // console.log(isuser);
+        if(!isuser){
+            return res.status(422).json({error:"Invalid credentials"})
+        };
+
+        const isPasswordCorrect = bcrypt.compareSync(password,isuser.password);
+
+        if(!isPasswordCorrect){
+            return res.status(422).json({error:"Invalid credentials"})
+        };
+        const jwtkey = process.env.JWTKEY;
+        const token = jwt.sign({id:isuser._id, name:isuser.name}, jwtkey, {expiresIn:'1d'})
+        return res.status(200).json({token, id:isuser._id, name:isuser.name});
+    } catch (error) {
+        console.log(error);
+        return res.status(422).json({error:"User Login Failed."})
+    }
 }
 
 const getUser = async (req, res) => {
